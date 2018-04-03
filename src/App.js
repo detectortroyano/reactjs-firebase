626d5a023e3240ac31db63f5afb1f7aff40e896f
Angel Ricardo Vega Hernandez <angelricardo.uthh@gmail.com>
2018-04-02 21:01:17 -0500

    Reactjs Firebase Google Auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      user: null
+    }
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({
+        user: user
+      })
+    });
+  }
+  handleAuth(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+    .then(result => console.log(`${result.user.email} ha iniciado session`))
+    .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+  handleLogout(){
+    firebase.auth().signOut()
+      .then(result => console.log(`Ha cerrado session`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));    
+  }
+  renderLoginButton(){
+    if(this.state.user){
+      return(
+        <div>
+          <img src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p>Hola {this.state.user.displayName}!</p>
+          <button onClick={this.handleLogout}>Salir</button>
+        </div>
+      );
+    }else{
+      return(
+        <button onClick={this.handleAuth}>Login con Google</button>
+      );      
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +55,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          { this.renderLoginButton() }
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAJvAAIrO164dLOko5weQGbwCAWdSV6Bb4",
+  authDomain: "reactjs-firebase-a73f3.firebaseapp.com",
+  databaseURL: "https://reactjs-firebase-a73f3.firebaseio.com",
+  projectId: "reactjs-firebase-a73f3",
+  storageBucket: "reactjs-firebase-a73f3.appspot.com",
+  messagingSenderId: "840369610669"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
